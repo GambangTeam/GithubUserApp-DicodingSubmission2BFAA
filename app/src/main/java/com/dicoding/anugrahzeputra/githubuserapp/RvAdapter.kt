1cddcb761d915ae49f0573359d943f5a7ebe2f2d
anugrahzeputra <anugrahzeputra@gmail.com>
2021-06-08 12:34:11 +0700

    fixing binding object into RvAdapter

--- app/src/main/java/com/dicoding/anugrahzeputra/githubuserapp/RvAdapter.kt
+++ app/src/main/java/com/dicoding/anugrahzeputra/githubuserapp/RvAdapter.kt
@@ -1,5 +1,6 @@
 package com.dicoding.anugrahzeputra.githubuserapp
 
+import android.opengl.GLES30
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +11,67 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.dicoding.anugrahzeputra.githubuserapp.databinding.ItemviewBinding
 
 class RvAdapter(private val listGithubUser: ArrayList<GithubUser>) : RecyclerView.Adapter<RvAdapter.CardViewViewHolder>(
 ) {
-    class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private lateinit var itemInClick : ItemInClickCallback
+    private lateinit var buttonInClick : ItemInClickCallback
 
+    interface ItemInClickCallback {
+        fun itemClicked(data: GithubUser)
+        fun buttonClicked(data: GithubUser)
     }
+
+    fun setItemInClick(itemInClickCallback: ItemInClickCallback) {
+        this.itemInClick = itemInClickCallback
+        this.buttonInClick = itemInClickCallback
+    }
+
+    inner class CardViewViewHolder(private val binding : ItemviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(users: GithubUser) {
+            with(binding) {
+                Glide.with(itemView.context)
+                    .load(users.photo)
+                    .apply(RequestOptions().override(350, 550))
+                    .into(imgItemPhoto)
+                tvItemName.text = users.name
+                tvUserId.text = users.id
+                val follower = "Follower = " + users.follower
+                tvDesc.text = follower
+                val following = "Following = " + users.following
+                tvDesc.text = following
+                itemView.setOnClickListener {
+                    itemInClick.itemClicked(users)
+                    Toast.makeText(
+                        itemView.context,
+                        "Kamu memilih " + users.name,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                btnShare.setOnClickListener {
+                    buttonInClick.buttonClicked(users)
+                    Toast.makeText(
+                        itemView.context,
+                        "Go to Github user " + users.name,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
+        val binding = ItemviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CardViewViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardViewViewHolder, position: Int) {
+        holder.bind(listGithubUser[position])
+    }
+
+    override fun getItemCount(): Int = listGithubUser.size
 
     /*private lateinit var itemInClick : ItemInClickCallback
     private lateinit var buttonInClick : ItemInClickCallback
